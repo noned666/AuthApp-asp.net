2eec4c3f1c37516c1b80de3ad51d69d213a9d1a1
Константин Калмыков <Kleymoments@gmail.com>
2022-07-28 19:45:37 +0300

    Добавьте файлы проекта.

---
+++ AuthApp/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AuthApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return Content(User.Identity.Name);
+        }
+    }
+}
